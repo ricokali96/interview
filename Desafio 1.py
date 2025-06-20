fe2ea324c9867ff39a167850f3b840b50aa1f871
ricokali96 <rico96lira@gmail.com>
2025-06-20 15:16:14 -0300

    First Interview

---
+++ Desafio 1.py
@@ -0,0 +1,13 @@
+# Faça um programa que é capaz de gerenciar o inventario de livros em uma biblioteca.
+# O programa precisa ter um inventario inicial de 8 livros.
+#   O Magico de Oz, O Poder do habito, Orgulho e Preconceito, Crime e Castigo, Antifrágel, Todos os caminhos levam a roma, A branca de Neve, Moby Dick
+
+
+# O usuário pode ter 2 opções: emprestar o livro, devolver.
+
+# O usuário não sabe qual livro tem na biblioteca, o programa precisa mostrar para ele quais são as opções para que o usuário pode escolher...
+## o livro para empréstimo
+
+# Quando o usuário for devolver o livro, o programa deve mostrar qual livro que falta e pedir para o usuário confirmar qual ele quer devolver.
+
+
